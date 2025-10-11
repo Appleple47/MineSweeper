8a9827082d0c2a4aaa2017a00d1b98c673527ef4
Rintaro Abe <apple.rin5.go@gmail.com>
2025-10-12 06:20:38 +0900

    デプロイパスの修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/MineSweeper/', 
   plugins: [react()],
+  base: '/MineSweeper/', 
   build: {
-    outDir: '../docs', 
-    emptyOutDir: true,
-  }
-})
+    outDir: 'dist', 
+  },
+});
